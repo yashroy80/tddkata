b280e1a59c22fc44fec3855e7a2699addcb0070c
Yash <80704137+yashroy80@users.noreply.github.com>
2024-09-08 14:28:42 +0530

    Added Test Cases

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -26,10 +22,71 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Please enter comma separated numbers!');
   });
 
-  it('should render title', () => {
+  it(`should have sum as 0 on app load`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    let input: HTMLInputElement = fixture.nativeElement.querySelector('input');
+    expect(input.value).toBe('');
+    expect(app.result).toEqual(0);
+  });
+
+  it(`should output 0 when inserting space/empty text`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tddkata app is running!');
+    const app = fixture.componentInstance;
+    let input: HTMLInputElement = fixture.nativeElement.querySelector('input');
+    input.value = '';
+    input.dispatchEvent(new Event('input'));
+    expect(app.result).toEqual(0);
+  });
+
+  it(`should output 1 when input is 1`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.componentInstance;
+    let input: HTMLInputElement = fixture.nativeElement.querySelector('input');
+    input.value = '1';
+    input.dispatchEvent(new Event('input'));
+    expect(app.result).toEqual(1);
+  });
+
+  it(`should output 6 when input is 1,5`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.componentInstance;
+    let input: HTMLInputElement = fixture.nativeElement.querySelector('input');
+    input.value = '1,5';
+    input.dispatchEvent(new Event('input'));
+    expect(app.result).toEqual(6);
+  });
+
+  it(`should handle newline characters, output should be 6 when input is '1\\n2,3'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.componentInstance;
+    let input: HTMLInputElement = fixture.nativeElement.querySelector('input');
+    input.value = '1\\n2,3';
+    input.dispatchEvent(new Event('input'));
+    expect(app.result).toEqual(6);
+  });
+
+  it(`should support different delimiters, output should be 3 when input is '//;\\n1;2'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.componentInstance;
+    let input: HTMLInputElement = fixture.nativeElement.querySelector('input');
+    input.value = '//;\\n1;2';
+    input.dispatchEvent(new Event('input'));
+    expect(app.result).toEqual(3);
+  });
+
+  it(`should throw error when negative numbers are inserted'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.componentInstance;
+    let input: HTMLInputElement = fixture.nativeElement.querySelector('input');
+    input.value = '1,-2';
+    input.dispatchEvent(new Event('input'));
+    expect(app.error).toEqual('negative numbers not allowed -2');
   });
 });
